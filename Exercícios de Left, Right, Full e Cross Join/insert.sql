2bc90d11ce88c6cec351972e9976691194c9140c
Cadu_JVN <145131105+cadu01234@users.noreply.github.com>
2024-08-13 08:01:35 -0300

    insert.sql

---
+++ Exercícios de Left, Right, Full e Cross Join/insert.sql
@@ -0,0 +1,29 @@
+INSERT INTO tbAutor VALUES
+('Chico Buarque'),
+('José Amado'),
+('J.K. Rowling'),
+('Willam Shakespeare'),
+('Monteiro Lobato'),
+('Cora Coraline'),
+('Clarice Lispector')
+
+INSERT INTO tbGenero VALUES
+('Ficção'),
+('Romance'),
+('Tragédia')
+
+INSERT INTO tbEditora VALUES
+('Melhoramentos'),
+('Globo'),
+('Ática'),
+('Companhia das letras'),
+('Abril')
+
+INSERT INTO tbLivro VALUES
+('Budapeste', '176', '2', '4', '1'),
+('O Bicho-da-Seda', '454', '2', '5', '3'),
+('Gabriela, Cravo e Canela', '214', '1', '2', '2'),
+('Reinações de Narizinho', '143', '2', '2', '5'),
+('Romeu e Julieta', '421', '1', '3', '4'),
+('O Irmão Alemão', '478', '1', '4', '1'), ('Terras do Sem Fim', '125', '1', '3', '2'),
+('Animais Fantáticos e Onde Habitam', '267', '2', '4', '3')
